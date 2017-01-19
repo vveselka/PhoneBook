412b7b73105351f2f09a70bd35a7898050a2e922
Victoria Yaremenko <victoria.yaremenko@gmail.com>
2017-01-19 11:51:57 +0000

    refactor app.js

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
-var ReactDOM = require('react-dom');
-var React = require('react');
-var PhoneBook = require('./PhoneBook');
+import ReactDOM from 'react-dom';
+import React from 'react';
+import PhoneBook from './PhoneBook';
 
 $.ajax({
   url: 'http://www.mocky.io/v2/581335f71000004204abaf83',
   type: 'get',
   dataType: 'jsonp',
-}).done(function(data) {
-  ReactDOM.render(<PhoneBook contacts={data.contacts} />, document.getElementById('app'));
-}.bind(this)).fail(function(data) {
-  if(data.statusText) {
-    console.log(data.statusText);
-  }
-});
+}).done((data) => ReactDOM.render(<PhoneBook contacts={data.contacts} />, document.getElementById('app')))
+  .fail((data) => console.log('Error ' + data.status));
